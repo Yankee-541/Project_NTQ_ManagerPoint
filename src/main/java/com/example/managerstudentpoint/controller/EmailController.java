3bab5128416a02770387d96d05768a2628ebadcb
D U Y Đ Ă N G <dangtd5401@gmail.com>
2022-07-11 11:44:05 +0700

    test commit

--- src/main/java/com/example/managerstudentpoint/controller/EmailController.java
+++ src/main/java/com/example/managerstudentpoint/controller/EmailController.java
@@ -3,16 +3,14 @@ package com.example.managerstudentpoint.controller;
 import com.example.managerstudentpoint.dto.UserDTO;
 import com.example.managerstudentpoint.service.AuthenService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.mail.*;
-import javax.mail.internet.*;
+import javax.mail.MessagingException;
 import java.io.IOException;
-import java.util.Date;
-import java.util.Properties;
 
 @RestController
 @RequestMapping(value = "api/student")
@@ -23,7 +21,6 @@ public class EmailController {
 
     @PostMapping("/forgot-password")
     public ResponseEntity<?> forgotPasswordSendToMail(@RequestBody UserDTO userDTO) throws MessagingException, IOException {
-        System.out.println();
         return authenService.forgotPasswordSendToMail(userDTO);
     }
 }
